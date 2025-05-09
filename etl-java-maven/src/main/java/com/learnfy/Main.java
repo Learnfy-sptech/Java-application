096b563f97474171839831316bbe406538a16fea
lucasaiello-dev <lucas.aiello@sptech.school>
2025-05-08 23:50:31 -0300

    refactor ProcessadorFactory for any mapped file

--- etl-java-maven/src/main/java/com/learnfy/Main.java
+++ etl-java-maven/src/main/java/com/learnfy/Main.java
@@ -2,14 +2,10 @@ package com.learnfy;
 
 import com.learnfy.logs.LogService;
 import com.learnfy.processador.*;
-import org.apache.commons.math3.analysis.function.Log;
 import org.springframework.jdbc.core.JdbcTemplate;
 import software.amazon.awssdk.services.s3.S3Client;
 import com.learnfy.s3.S3Service;
-
 import java.util.List;
-
-import static com.learnfy.processador.ProcessadorFactory.inserirDadosEscolaridade;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,28 +15,20 @@ public class Main {
         JdbcTemplate jdbcTemplate = ConexaoBanco.getJdbcTemplate();
         LogService logService = new LogService(jdbcTemplate);
 
-        /*
-        Altere o valor dessa variável para false caso seja a
-        primeira inserção ou queira inserir todos os dados novamente
-         */
-        Boolean primeiravez = true;
+        List<String> arquivos = S3Service.listarArquivos(bucket, prefixo);
 
-        if (primeiravez) {
-            ProcessadorFactory.inserirDadosEscolaridade(jdbcTemplate, s3Client, bucket, logService);
-        } else {
-            List<String> arquivos = S3Service.listarArquivos(bucket, prefixo);
+        if (!arquivos.isEmpty()) {
+            String arquivoMaisRecente = arquivos.getFirst();
 
-            if (!arquivos.isEmpty()) {
-                String arquivoMaisRecente = arquivos.getFirst();
-
-                if (arquivoMaisRecente.endsWith(".xlsx") || arquivoMaisRecente.endsWith(".xls") || arquivoMaisRecente.endsWith(".csv")) {
-                    Processador processador = ProcessadorFactory.getProcessador(arquivoMaisRecente, jdbcTemplate, s3Client, bucket, logService);
-                    try {
-                        processador.processar(bucket, arquivoMaisRecente);
-                    } catch (Exception e) {
-                        System.out.println(String.format("Houve um erro ao inserir os dados da tabela %s", arquivoMaisRecente));
-                    }
+            if (arquivoMaisRecente.endsWith(".xlsx")) {
+                Processador processador = ProcessadorFactory.getProcessador(arquivoMaisRecente, jdbcTemplate, s3Client, bucket, logService);
+                try {
+                    processador.processar(bucket, arquivoMaisRecente);
+                } catch (Exception e) {
+                    System.out.println(String.format("Houve um erro ao inserir os dados da tabela %s", arquivoMaisRecente));
                 }
+            } else {
+                logService.registrarLog("Main.java", "Indefinido", "CRÍTICO", "Tipo de arquivo inválido");
             }
         }
     }
--- etl-java-maven/src/main/java/com/learnfy/processador/ProcessadorFactory.java
+++ etl-java-maven/src/main/java/com/learnfy/processador/ProcessadorFactory.java
@@ -4,51 +4,22 @@ import com.learnfy.logs.LogService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProcessadorFactory {
     public static Processador getProcessador(String key, JdbcTemplate jdbcTemplate, S3Client s3Client, String bucketName, LogService logService) {
-        if (key.startsWith("planilhas/dados_cursos/")) {
-            return new ProcessadorCursoOfertado(jdbcTemplate, s3Client, logService);
-        } else if (key.startsWith("planilhas/dados_empregabilidade/")) {
-            return new ProcessadorEmpregabilidade(jdbcTemplate, s3Client, bucketName, logService);
-        } else {
-            throw new IllegalArgumentException("Tipo de arquivo desconhecido para a key: " + key);
-        }
-    }
-
-    public static void inserirDadosEscolaridade(JdbcTemplate jdbcTemplate, S3Client s3Client, String bucket, LogService logService) {
-        Processador processadorUf = new ProcessadorUf(jdbcTemplate, s3Client, logService);
-        try {
-            processadorUf.processar(bucket, "planilhas/dados_cursos/estados.xlsx");
-        } catch (Exception e) {
-            System.out.println(String.format("Não foi possível processar os dados dos Estados, erro: %s", e.getMessage()));
-        }
-
-        Processador processadorMunicipio = new ProcessadorMunicipio(jdbcTemplate, s3Client, logService);
-        try {
-            processadorMunicipio.processar(bucket, "planilhas/dados_cursos/municipios.xlsx");
-        } catch (Exception e) {
-            System.out.println(String.format("Não foi possível processar os dados dos Municípios, erro: %s", e.getMessage()));
-        }
-
-        Processador processadorIes = new ProcessadorIes(jdbcTemplate, s3Client, logService);
-        try {
-            processadorIes.processar(bucket, "planilhas/dados_cursos/ies.xlsx");
-        } catch (Exception e) {
-            System.out.println(String.format("Não foi possível processar os dados das Instituições de Ensino, erro: %s", e.getMessage()));
-        }
-
-        Processador processadorCurso = new ProcessadorCursoArea(jdbcTemplate, s3Client, logService);
-        try {
-            processadorCurso.processar(bucket, "planilhas/dados_cursos/cursos_areas.xlsx");
-        } catch (Exception e) {
-            System.out.println(String.format("Não foi possível processar os dados dos Cursos e das Áreas, erro: %s", e.getMessage()));
-        }
-
-        Processador processadorCursoOfertado = new ProcessadorCursoOfertado(jdbcTemplate, s3Client, logService);
-        try {
-            processadorCursoOfertado.processar(bucket, "planilhas/dados_cursos/cursos_ofertados.xlsx");
-        } catch (Exception e) {
-            System.out.println("Não foi possível processar os dados dos Cursos Ofertados");
-        }
+         return switch (key) {
+            case "planilhas/dados_empregabilidade/empregabilidade.xlsx" -> new ProcessadorEmpregabilidade(jdbcTemplate, s3Client, bucketName, logService);
+            case "planilhas/dados_cursos/estados.xlsx" -> new ProcessadorUf(jdbcTemplate, s3Client, logService);
+            case "planilhas/dados_cursos/municipios.xlsx" -> new ProcessadorMunicipio(jdbcTemplate, s3Client, logService);
+            case "planilhas/dados_cursos/instituicoes_ensino.xlsx" -> new ProcessadorIes(jdbcTemplate, s3Client, logService);
+            case "planilhas/dados_cursos/cursos_areas.xlsx" -> new ProcessadorCursoArea(jdbcTemplate, s3Client, logService);
+            case "planilhas/dados_cursos/cursos_ofertados.xlsx" -> new ProcessadorCursoOfertado(jdbcTemplate, s3Client, logService);
+             default -> {
+                 logService.registrarLog("ProcessadorFactory.java", "ProcessadorFactory", "CRÍTICO", "Nome do arquivo inválido");
+                 yield null;
+             }
+         };
     }
 }
